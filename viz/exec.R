24659d5f0b0fcb2d5b8ea94856606dfd98da8fd5
notyourbiz <notyourbiz>
2019-01-15 19:29:23 +0000

    Suppression du systeme de md5

--- viz/exec.R
+++ viz/exec.R
@@ -11,22 +11,11 @@ radius = 	plot_radius,
 avg_week =  plot_avg_week,
 pcount = plot_pcount) 
 
-try(load('tasks.RData'), silent=TRUE)
-if(!exists('tasks_md5')) { tasks_md5 <- list() }
-	
-
 compl <- function(f, x) {
-			d <- digest(as.character(body(f)));
-			if( d != tasks_md5[x]){
 				print(x %s+% " en cours de creation")
-				setwd("./build")
+				dir.create('img', showWarnings=F)
+				setwd("./img")
 				f(x)
-			} else {
-				print(x %s+% " deja fait")
-			}
-			return(d)
 			}
 	
-tasks_md5 <- list_modify(tasks_md5, !!! mcmapply(compl, tasks, names(tasks), mc.preschedule=TRUE, mc.cores=5))
-
-save(tasks_md5, file='tasks.RData')
+mcmapply(compl, tasks, names(tasks), mc.preschedule=TRUE, mc.cores=5)
